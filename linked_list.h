4338fbb4e42a8e78cd29ccc03026744e289bbfdf
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-02-10 21:54:22 +0100

    Added Node class

--- linked_list.h
+++ linked_list.h
@@ -1,4 +1,16 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
+template <class T>
+class Node {
+   public:
+    T data;
+    Node* next;
+
+    Node(T value) {
+        this->data = value;
+        this->next = nullptr;
+    }
+};
+
 #endif
