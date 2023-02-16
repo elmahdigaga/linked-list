86cb4906e1e0dc965a8cc67ae35fd30922225f38
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-02-16 17:20:27 +0100

    Changed variable names

--- linked_list.h
+++ linked_list.h
@@ -6,192 +6,192 @@
 template <class T>
 class Node {
    public:
-    T data_;
-    Node<T>* next_;
+    T data;
+    Node<T>* next;
 
     Node(T value) {
-        this->data_ = value;
-        this->next_ = nullptr;
+        this->data = value;
+        this->next = nullptr;
     }
 };
 
 template <class T>
 class LinkedList {
    private:
-    Node<T>* head_;
-    int size_;
+    Node<T>* head;
+    int size;
 
    public:
     LinkedList() {
-        head_ = nullptr;
-        size_ = 0;
+        head = nullptr;
+        size = 0;
     }
 
     ~LinkedList() {
-        Node<T>* temp = head_;
+        Node<T>* temp = head;
         while (temp != nullptr) {
             Node<T>* to_delete = temp;
-            temp = temp->next_;
+            temp = temp->next;
             delete to_delete;
         }
     }
 
     void Print() {
-        if (head_ == nullptr) {
+        if (head == nullptr) {
             std::cout << "List is empty\n";
             return;
         }
 
-        Node<T>* temp = head_;
+        Node<T>* temp = head;
         while (temp != nullptr) {
-            std::cout << temp->data_ << " -> ";
-            temp = temp->next_;
+            std::cout << temp->data << " -> ";
+            temp = temp->next;
         }
         std::cout << "NULL\n";
     }
 
     int Size() {
-        return size_;
+        return size;
     }
 
     LinkedList& InsertBegin(T value) {
         Node<T>* new_node = new Node(value);
-        new_node->next_ = head_;
-        head_ = new_node;
-        ++size_;
+        new_node->next = head;
+        head = new_node;
+        ++size;
         return *this;
     }
 
     LinkedList& InsertEnd(T value) {
         Node<T>* new_node = new Node(value);
-        if (head_ == nullptr) {
-            head_ = new_node;
-            ++size_;
+        if (head == nullptr) {
+            head = new_node;
+            ++size;
             return *this;
         }
 
-        Node<T>* temp = head_;
-        while (temp->next_ != nullptr) {
-            temp = temp->next_;
+        Node<T>* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
         }
-        temp->next_ = new_node;
-        ++size_;
+        temp->next = new_node;
+        ++size;
         return *this;
     }
 
     LinkedList& InsertPos(T value, int pos) {
-        if (pos > size_) {
+        if (pos > size) {
             std::cerr << "Invalid position\n";
             return *this;
         } else if (pos == 0) {
             InsertBegin(value);
             return *this;
-        } else if (pos == size_) {
+        } else if (pos == size) {
             InsertEnd(value);
             return *this;
         }
 
         Node<T>* new_node = new Node(value);
-        Node<T>* temp = head_;
+        Node<T>* temp = head;
         for (int i = 0; i < pos - 1; ++i) {
-            temp = temp->next_;
+            temp = temp->next;
         }
-        new_node->next_ = temp->next_;
-        temp->next_ = new_node;
-        ++size_;
+        new_node->next = temp->next;
+        temp->next = new_node;
+        ++size;
         return *this;
     }
 
     LinkedList& RemoveBegin() {
-        if (head_ == nullptr) {
+        if (head == nullptr) {
             std::cerr << "List is empty\n";
             return *this;
         }
 
-        Node<T>* temp = head_;
-        head_ = temp->next_;
+        Node<T>* temp = head;
+        head = temp->next;
         delete temp;
-        --size_;
+        --size;
         return *this;
     }
 
     LinkedList& RemoveEnd() {
-        if (head_ == nullptr) {
+        if (head == nullptr) {
             std::cerr << "List is empty\n";
             return *this;
         }
 
-        Node<T>* temp = head_;
-        if (head_->next_ == nullptr) {
-            head_ = nullptr;
+        Node<T>* temp = head;
+        if (head->next == nullptr) {
+            head = nullptr;
             delete temp;
-            --size_;
+            --size;
             return *this;
         }
         Node<T>* temp2;
-        while (temp->next_ != nullptr) {
+        while (temp->next != nullptr) {
             temp2 = temp;
-            temp = temp->next_;
+            temp = temp->next;
         }
-        temp2->next_ = nullptr;
+        temp2->next = nullptr;
         delete temp;
-        --size_;
+        --size;
         return *this;
     }
 
     LinkedList& RemovePos(int pos) {
-        if (pos >= size_) {
+        if (pos >= size) {
             std::cerr << "Invalid position\n";
             return *this;
         } else if (pos == 0) {
             RemoveBegin();
             return *this;
-        } else if (pos == size_ - 1) {
+        } else if (pos == size - 1) {
             RemoveEnd();
             return *this;
         }
 
-        Node<T>* temp = head_;
+        Node<T>* temp = head;
         Node<T>* temp2;
         for (int i = 0; i < pos; ++i) {
             temp2 = temp;
-            temp = temp->next_;
+            temp = temp->next;
         }
-        temp2->next_ = temp->next_;
+        temp2->next = temp->next;
         delete temp;
-        --size_;
+        --size;
         return *this;
     }
 
     T Get(int pos) {
-        if (head_ == nullptr) {
+        if (head == nullptr) {
             std::cerr << "List is empty\n";
             return 0;
         }
-        if (pos >= size_) {
+        if (pos >= size) {
             std::cerr << "Invalid index\n";
             return 0;
         }
 
-        Node<T>* temp = head_;
+        Node<T>* temp = head;
         for (int i = 0; i < pos; ++i) {
-            temp = temp->next_;
+            temp = temp->next;
         }
-        return temp->data_;
+        return temp->data;
     }
 
     int Find(T value) {
-        if (head_ == nullptr) {
+        if (head == nullptr) {
             std::cerr << "List is empty\n";
             return -1;
         }
         int pos = 0;
-        Node<T>* temp = head_;
+        Node<T>* temp = head;
         while (temp != nullptr) {
-            if (temp->data_ == value) {
+            if (temp->data == value) {
                 return pos;
             }
-            temp = temp->next_;
+            temp = temp->next;
             ++pos;
         }
 
@@ -199,7 +199,7 @@ class LinkedList {
     }
 
     LinkedList& Delete(T value) {
-        if (head_ == nullptr) {
+        if (head == nullptr) {
             std::cerr << "List is empty\n";
             return *this;
         }
@@ -214,20 +214,20 @@ class LinkedList {
     }
 
     bool IsEmpty() {
-        return head_ == nullptr ? true : false;
+        return head == nullptr ? true : false;
     }
 
     T* ToArray() {
-        if (head_ == nullptr) {
+        if (head == nullptr) {
             std::cerr << "List is empty\n";
             return nullptr;
         }
 
-        T* array = new T[size_];
-        Node<T>* temp = head_;
-        for (int i = 0; i < size_; ++i) {
-            array[i] = temp->data_;
-            temp = temp->next_;
+        T* array = new T[size];
+        Node<T>* temp = head;
+        for (int i = 0; i < size; ++i) {
+            array[i] = temp->data;
+            temp = temp->next;
         }
         return array;
     }
